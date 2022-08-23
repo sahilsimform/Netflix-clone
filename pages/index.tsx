a3a1f92eaa21dadb4dd3e4be6918f41aee46d5c7
sahilsimform <sahilsinghsimform@gmail.com>
2022-08-23 14:59:05 +0530

    Completing UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,24 +28,23 @@ const Home = ({
   trendingNow,
 }: Props) => {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b  lg:h-[140vh]">
       <Head>
-        <title>Create Next App</title>
+        <title>Sahil NetFlex</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main className="relative pl-4 pb-24 lg:space-x-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
-        <section>
-          {/* <Row title="Trending Now" movies={trendingNow} />
+        <section className="md:space-y-24">
+          <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
-          <Row title="Action Thri" movies={actionMovies} />
-          {list.length > 0 && <Row title="My List" movies={list} />}
+          <Row title="Action Thrillers" movies={actionMovies} />
 
           <Row title="Comedies" movies={comedyMovies} />
-          <Row title="Scary Mo" movies={horrorMovies} />
-          <Row title="" movies={} />
-          <Row title="" movies={} /> */}
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
     </div>
